37854626d862aa5ccb963025dafe7cecdcadf70d
Won-Wook Hong <jradoo@gmail.com>
2018-12-30 15:30:21 +0900

    added semantic ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+//import 'semantic-ui-css/semantic.min.css' // need loaders to work
+import { Container } from 'semantic-ui-react'
 
 
 ReactDOM.render(
-    <div>Hello world</div>,
+    <Container><p>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+    Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+    consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+    link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+    vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+    enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
+    ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+    Curabitur ullamcorper ultricies nisi.
+  </p></Container>
+    ,
     document.getElementById("example")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,8 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
+            { test: /\.css$/, loader: "css-loader", options: { sourceMap: true} },
+
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
